83f6ec16fa9461b702816f44ccd84865278bbabb
sekigon-gonnoc <43873124+sekigon-gonnoc@users.noreply.github.com>
2021-06-28 22:42:41 +0900

    Trim mouse report ID before send

--- tmk_core/protocol/pico/iusb.c
+++ tmk_core/protocol/pico/iusb.c
@@ -11,13 +11,6 @@ uint8_t keyboard_leds(void) {
 }
 
 void send_keyboard(report_keyboard_t *report) {
-    static report_keyboard_t last_report = {0};
-    if (memcmp(&last_report, report, sizeof(last_report)) == 0) {
-        return;
-    } else {
-        last_report = *report;
-    }
-
     while (!tud_hid_n_ready(ITF_NUM_HID_KEYBOARD)) {
         tud_task();
         if (!tud_ready()) {
@@ -25,30 +18,19 @@ void send_keyboard(report_keyboard_t *report) {
         }
     }
     tud_hid_n_report(ITF_NUM_HID_KEYBOARD, 0, report, sizeof(*report));
+    tud_task();
 }
 
 void send_mouse(report_mouse_t *report) {
-    static report_mouse_t last_report = {0};
-    if (memcmp(&last_report, report, sizeof(last_report)) == 0) {
-        if (last_report.h == 0 && last_report.v == 0 && last_report.x == 0 &&
-            last_report.y == 0) {
-            return;
-        }
-    } else {
-        last_report = *report;
-    }
-
-
-    if (!tud_ready()) {
-        return;
-    }
     while (!tud_hid_n_ready(ITF_NUM_HID_MOUSE)) {
         tud_task();
         if (!tud_ready()) {
             return;
         }
     }
-    tud_hid_n_report(ITF_NUM_HID_MOUSE, 0, report, sizeof(*report));
+    // Require only 5 bytes. Do not usb report->id
+    tud_hid_n_report(ITF_NUM_HID_MOUSE, 0, &report->buttons, 5);
+    tud_task();
 }
 
 void send_system(uint16_t data) {
@@ -59,6 +41,7 @@ void send_system(uint16_t data) {
         }
     }
     tud_hid_n_report(ITF_NUM_HID_EXTRA, REPORT_ID_SYSTEM, &data, 2);
+    tud_task();
 }
 void send_consumer(uint16_t data) {
     while (!tud_hid_n_ready(ITF_NUM_HID_EXTRA)) {
@@ -68,4 +51,5 @@ void send_consumer(uint16_t data) {
         }
     }
     tud_hid_n_report(ITF_NUM_HID_EXTRA, REPORT_ID_CONSUMER, &data, 2);
+    tud_task();
 }
