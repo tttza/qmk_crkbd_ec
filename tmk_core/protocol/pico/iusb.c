3ce92d2765e4285e364a9851f06a1dafd4e6ceb5
sekigon-gonnoc <43873124+sekigon-gonnoc@users.noreply.github.com>
2021-06-28 22:13:58 +0900

    Fix dprint hang on Windows

--- tmk_core/protocol/pico/iusb.c
+++ tmk_core/protocol/pico/iusb.c
@@ -11,40 +11,61 @@ uint8_t keyboard_leds(void) {
 }
 
 void send_keyboard(report_keyboard_t *report) {
-    if (!tud_ready()) {
+    static report_keyboard_t last_report = {0};
+    if (memcmp(&last_report, report, sizeof(last_report)) == 0) {
         return;
+    } else {
+        last_report = *report;
     }
+
     while (!tud_hid_n_ready(ITF_NUM_HID_KEYBOARD)) {
         tud_task();
+        if (!tud_ready()) {
+            return;
+        }
     }
     tud_hid_n_report(ITF_NUM_HID_KEYBOARD, 0, report, sizeof(*report));
 }
 
 void send_mouse(report_mouse_t *report) {
+    static report_mouse_t last_report = {0};
+    if (memcmp(&last_report, report, sizeof(last_report)) == 0) {
+        if (last_report.h == 0 && last_report.v == 0 && last_report.x == 0 &&
+            last_report.y == 0) {
+            return;
+        }
+    } else {
+        last_report = *report;
+    }
+
+
     if (!tud_ready()) {
         return;
     }
     while (!tud_hid_n_ready(ITF_NUM_HID_MOUSE)) {
         tud_task();
+        if (!tud_ready()) {
+            return;
+        }
     }
     tud_hid_n_report(ITF_NUM_HID_MOUSE, 0, report, sizeof(*report));
 }
 
 void send_system(uint16_t data) {
-    if (!tud_ready()) {
-        return;
-    }
     while (!tud_hid_n_ready(ITF_NUM_HID_EXTRA)) {
         tud_task();
+        if (!tud_ready()) {
+            return;
+        }
     }
     tud_hid_n_report(ITF_NUM_HID_EXTRA, REPORT_ID_SYSTEM, &data, 2);
 }
 void send_consumer(uint16_t data) {
-    if (!tud_ready()) {
-        return;
-    }
     while (!tud_hid_n_ready(ITF_NUM_HID_EXTRA)) {
         tud_task();
+        if (!tud_ready()) {
+            return;
+        }
     }
     tud_hid_n_report(ITF_NUM_HID_EXTRA, REPORT_ID_CONSUMER, &data, 2);
 }
--- tmk_core/protocol/pico/main.c
+++ tmk_core/protocol/pico/main.c
@@ -37,8 +37,34 @@
 //--------------------------------------------------------------------+
 static int cdc_in_chars(char* buf, int len) { return 0; }
 
-static stdio_driver_t stdio_driver = {.out_chars = tud_cdc_write,
-                                      .out_flush = tud_cdc_write_flush,
+static void cdc_write(const char* buf, int32_t length) {
+    static uint64_t last_avail_time;
+    if (tud_cdc_connected()) {
+        for (int i = 0; i < length;) {
+            int n     = length - i;
+            int avail = tud_cdc_write_available();
+            if (n > avail) n = avail;
+            if (n) {
+                int n2 = tud_cdc_write(buf + i, n);
+                tud_task();
+                tud_cdc_write_flush();
+                i += n2;
+                last_avail_time = time_us_64();
+            } else {
+                tud_task();
+                tud_cdc_write_flush();
+                if (!tud_cdc_connected() ||
+                    (!tud_cdc_write_available() &&
+                     time_us_64() > last_avail_time + 500000)) {
+                    break;
+                }
+            }
+        }
+    }
+}
+
+static stdio_driver_t stdio_driver = {.out_chars = cdc_write,
+                                      .out_flush = NULL,
                                       .in_chars  = cdc_in_chars,
                                       .next      = NULL};
 
