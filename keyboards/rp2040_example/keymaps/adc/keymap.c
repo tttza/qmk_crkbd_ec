9380a72c4ecd2c0b58f22f99a3173c9d16da5c2f
sekigon-gonnoc <43873124+sekigon-gonnoc@users.noreply.github.com>
2022-01-16 23:09:12 +0900

    Fix #47: Remove pico_eeprom.h from include files

--- keyboards/rp2040_example/keymaps/adc/keymap.c
+++ keyboards/rp2040_example/keymaps/adc/keymap.c
@@ -14,7 +14,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
-#include "pico_eeprom.h"
 #include "analog.h"
 
 // Defines names for use in layer keycodes and the keymap
