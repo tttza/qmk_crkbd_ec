f1c7ab7571bd3667a4e6439e7daadf78c66a753c
tttza <25410554+tttza@users.noreply.github.com>
2025-12-28 15:44:17 +0900

    feat: enable permissive hold for improved key press handling

--- keyboards/sekigon/crkbd_ec/config.h
+++ keyboards/sekigon/crkbd_ec/config.h
@@ -114,6 +114,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #    define RGB_DISABLE_WHEN_USB_SUSPENDED
 #    define RGB_MATRIX_FRAMEBUFFER_EFFECTS
 #    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150
+#    define RGB_MATRIX_TIMEOUT 900000
 #    define RGB_MATRIX_HUE_STEP 8
 #    define RGB_MATRIX_SAT_STEP 8
 #    define RGB_MATRIX_VAL_STEP 8
